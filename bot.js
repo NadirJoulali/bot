5e7d4f757fcb433dd607a36a5933a50855a337f1
NadirJoulali <39174113+NadirJoulali@users.noreply.github.com>
2018-08-15 16:03:04 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1498,7 +1498,7 @@ client.on('message', message => {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "S Bot";
+    let copy = "Only Pro Bot ";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
@@ -1519,7 +1519,6 @@ let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
        .addField('Server', message.guild.name)
        .addField('Sender', message.author.username)
        .addField('Message', args)
-       .setImage("https://imgur.com/Eqo0SM1")
        .setThumbnail(message.author.avatarURL)
        .setFooter(copy, client.user.avatarURL);
     m.send({ embed: bc })
