c43829103310bb34513a60127b4489f97bfe6cfe
NadirJoulali <39174113+NadirJoulali@users.noreply.github.com>
2018-09-26 18:52:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,43 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on("ready", () => {
+console.log("Welcome Again !");
 
+ })
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم يا قلبي 
+لو سمحت طالبق طلب تكقا ادخل السيرفر
+    https://discord.gg/tWqsMy  
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`)
+}).catch(console.error)
 
-client.login("NDg3MzM0NzMxMzQ4ODM2MzUy.DnMLTg.wAPcdGGic9C_8oY62VGIQbnza0c");
+})
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم يا قلبي 
+لو سمحت طالبق طلب تكقا ادخل السيرفر
+ https://discord.gg/tWqsMy  
+الدعووة خاصة لك ي قلبي ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+
+client.on("ready", () => {
+
+let channel = client.channels.get("494555815672414218"); 
+       setInterval(() => {
+channel.send(Math.random().toString(36).substring(7))
+
+     
+      
+},10);
+});
+
+})
+client.login("NDkxOTMwODAxODM2MTMwMzA1.Do1NIw.1-CsakC1bE7AQyNDoWFaCDZR_pU");
