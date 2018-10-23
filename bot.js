f779666d56026d5063c1031005540019b56b0819
NadirJoulali <39174113+NadirJoulali@users.noreply.github.com>
2018-10-23 18:37:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,117 +1,68 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const Matra9a = new Discord.Client();
+const fs = require('fs');
+const suck = JSON.parse(fs.readFileSync('./suck.json', 'utf8'));
+const prefix = "!";
 
 
-/*	
- 
-Codes server - !.Zeyad' , ♚Roýale#5555
-                                                                                                            
-                                                                                                            
-  ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
- /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
-                                                                                                            
-                                                                                                            
-		
-__________              __________        __   
-\______   \_______  ____\______   \ _____/  |_ 
- |     ___/\_  __ \/  _ \|    |  _//  _ \   __\
- |    |     |  | \(  <_> )    |   (  <_> )  |  
- |____|     |__|   \____/|______  /\____/|__|  
-                                \/             
-_________                    .___.__  __          
-\_   ___ \_______   ____   __| _/|__|/  |_  ______
-/    \  \/\_  __ \_/ __ \ / __ | |  \   __\/  ___/
-\     \____|  | \/\  ___// /_/ | |  ||  |  \___ \ 
- \______  /|__|    \___  >____ | |__||__| /____  >
-        \/             \/     \/               \/ 
-Codes server - !.Zeyad' , ♚Roýale#5555
-   _____  .__                     
-  /     \ |__| ____   ___________ 
- /  \ /  \|  |/    \_/ __ \_  __ \
-/    Y    \  |   |  \  ___/|  | \/
-\____|__  /__|___|  /\___  >__|   
-        \/        \/     \/       
-*/
+Matra9a.on('ready', () => {
+  console.log(`Logged in as ${Matra9a.user.tag}!`);
+Matra9a.user.setGame(`Rainbow Bot Matra9a`,"http://twitch.tv/Nadir44king")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${Matra9a.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${Matra9a.guilds.size} " ]`);
+  console.log(`Users! [ " ${Matra9a.users.size} " ]`);
+  console.log(`channels! [ " ${Matra9a.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Matra9a Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+Matra9a.on("message", message => {
+    fs.writeFile('./suck.json', JSON.stringify(suck));
+});
+Matra9a.on('ready', () => {
+    setInterval(function(){
+        Matra9a.guilds.forEach(g => {
+            if (suck[g.id]) {
+                if (suck[g.id].role) {
+                    var role = g.roles.get(suck[g.id].role);
+                    if (role) {
+                        role.edit({color : "RANDOM"});
+                    };
+                };
+            };
+        });
+    }, 10000);
+});
+Matra9a.on("message", message => {
+    if (!message.content.startsWith(prefix)) return;
+    if (message.author.bot) return;
+    if (message.channel.type !== "text") return message.reply("This Command Is Only Allowed In Servers");
+    var args = message.content.split(" ");
+    var command = args[0].slice(prefix.length);
+    switch(command) {
+        case "rainbow" :
+        if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("no no");
+        message.guild.createRole({name : "rainbow", color : "RANDOM"}).then(r => {
+            r.edit({color : "RANDOm"});
+            suck[message.guild.id] = {role : r.id};
+        });
+    };
+});
 
 
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-// Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// Codes Server` - iiZeyad , Roýale"1.5k 🍷#1111
-
-client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-    });
-}
-
-var timer = setTimeout(timerFunc, 1000);
-client.login(process.env.BOT_TOKEN);var ServerID = "502862540213321762"; //اي دي السيرفر
-var ChannelID = "503552912514088960";// اي دي الروم
-
-/*	
- 
-Codes server - !.Zeyad' , ♚Roýale#5555
-                                                                                                            
-                                                                                                            
-  ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
- /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
-                                                                                                            
-                                                                                                            
-		
-__________              __________        __   
-\______   \_______  ____\______   \ _____/  |_ 
- |     ___/\_  __ \/  _ \|    |  _//  _ \   __\
- |    |     |  | \(  <_> )    |   (  <_> )  |  
- |____|     |__|   \____/|______  /\____/|__|  
-                                \/             
-_________                    .___.__  __          
-\_   ___ \_______   ____   __| _/|__|/  |_  ______
-/    \  \/\_  __ \_/ __ \ / __ | |  \   __\/  ___/
-\     \____|  | \/\  ___// /_/ | |  ||  |  \___ \ 
- \______  /|__|    \___  >____ | |__||__| /____  >
-        \/             \/     \/               \/ 
-Codes server - !.Zeyad' , ♚Roýale#5555
-   _____  .__                     
-  /     \ |__| ____   ___________ 
- /  \ /  \|  |/    \_/ __ \_  __ \
-/    Y    \  |   |  \  ___/|  | \/
-\____|__  /__|___|  /\___  >__|   
-        \/        \/     \/       
-*/
-
-
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-// Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// Codes Server` - iiZeyad , Roýale"1.5k 🍷#1111
-
-client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
-
-
-    });
-}
-
-var timer = setTimeout(timerFunc, 1000);
-
-client.login(client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
